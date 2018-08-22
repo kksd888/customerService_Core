52c0a330c41e9d49cf703dd6e78406f49886b736
likeli <13120168539@163.com>
2018-08-22 17:57:55 +0800

    update ...

--- wechat/api.go
+++ wechat/api.go
@@ -1,20 +1,18 @@
 package wechat
 
+// 微信消息处理器，处理和微信服务器的交互
 type WeChat struct {
 	token string
 }
 
 // 获取微信授权
 func (wc *WeChat) getAccessToken() {
-
 }
 
 // 接受微信发送过来的数据
 func (wc *WeChat) receiveWeChatMsg() {
-
 }
 
 // 发送消息给微信
 func (wc *WeChat) sendWeChatMsg() {
-
 }
