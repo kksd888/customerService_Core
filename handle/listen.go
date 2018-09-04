6424d9e84b25d12b69f6ec881ebb775d0aee2eb1
likeli <13120168539@163.com>
2018-09-04 12:34:13 +0800

    * 超时下线

--- handle/listen.go
+++ handle/listen.go
@@ -1,9 +1,12 @@
 package handle
 
 import (
+	"git.jsjit.cn/customerService/customerService_Core/controller"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 var (
@@ -19,5 +22,21 @@ func Listen() {
 		<-sigs
 		// todo 所有客服强制下线 ？
 		os.Exit(0)
+	}()
+
+	// 客服超时下线
+	go func() {
+		for {
+			time.Sleep(time.Second * 1)
+			for k := range controller.OnlineKfs {
+				onLineKf := controller.OnlineKfs[k]
+				duration := time.Now().Sub(onLineKf.LastTime)
+				// 超时一小时则下线
+				if duration.Hours() > 1 {
+					log.Printf("客服[%s]超时，已经下线", onLineKf.KfId)
+					delete(controller.OnlineKfs, k)
+				}
+			}
+		}
 	}()
 }
