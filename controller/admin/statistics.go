2187ce1e7b777664f75f83d7bb374e20558af983
likeli <13120168539@163.com>
2019-03-08 11:58:24 +0800

    review

--- controller/admin/statistics.go
+++ controller/admin/statistics.go
@@ -2,13 +2,14 @@ package admin
 
 import (
 	"errors"
+	"net/http"
+	"strconv"
+	"time"
+
 	"git.jsjit.cn/customerService/customerService_Core/model"
 	"github.com/gin-gonic/gin"
 	"github.com/globalsign/mgo/bson"
 	log "github.com/sirupsen/logrus"
-	"net/http"
-	"strconv"
-	"time"
 )
 
 type StatisticsController struct {
@@ -18,32 +19,29 @@ func NewStatistics() *StatisticsController {
 	return &StatisticsController{}
 }
 
+// 统计查询
 func (c *StatisticsController) Statistics(context *gin.Context) {
-
-	//获取参数
 	var (
 		starTimeStr = context.Param("starTime")
 		endTimeStr  = context.Param("endTime")
 		pageStr     = context.Param("page")
 		limitStr    = context.Param("limit")
 	)
-	//判断参数
-
 	starTime, err := time.Parse("2006-01-02 15:04:05", starTimeStr)
 	if err != nil {
-		ReturnErrInfo1(context, errors.New("开始时间格式错误 格式应为 yyyy-MM-dd HH:mm:ss 如:2006-01-02 15:04:05"))
+		ReturnErrInfo(context, errors.New("开始时间格式错误 格式应为 yyyy-MM-dd HH:mm:ss 如:2006-01-02 15:04:05"))
 	}
 	endTime, err := time.Parse("2006-01-02 15:04:05", endTimeStr)
 	if err != nil {
-		ReturnErrInfo1(context, errors.New("结束时间格式错误,格式应为 yyyy-MM-dd HH:mm:ss 如:2006-01-02 15:04:05"))
+		ReturnErrInfo(context, errors.New("结束时间格式错误,格式应为 yyyy-MM-dd HH:mm:ss 如:2006-01-02 15:04:05"))
 	}
 	page, err := strconv.Atoi(pageStr)
 	if err != nil {
-		ReturnErrInfo1(context, errors.New("page类型错误,应为数字,如 1"))
+		ReturnErrInfo(context, errors.New("page类型错误,应为数字,如 1"))
 	}
 	limit, err := strconv.Atoi(limitStr)
 	if err != nil {
-		ReturnErrInfo1(context, errors.New("limit类型错误,应为数字,如 1000"))
+		ReturnErrInfo(context, errors.New("limit类型错误,应为数字,如 1000"))
 	}
 
 	//声明mongodb查询
@@ -126,16 +124,4 @@ func (c *StatisticsController) Statistics(context *gin.Context) {
 		messageByKf[i]["customerCount"] = count
 	}
 	context.JSON(http.StatusOK, messageByKf)
-}
-
-// 异常返回
-func ReturnErrInfo1(context *gin.Context, err interface{}) {
-	if err != nil {
-		log.Printf("发生异常：%#v", err)
-		context.JSON(http.StatusBadRequest, gin.H{
-			"code": http.StatusBadRequest,
-			"msg":  err.(error).Error(),
-		})
-		panic(err)
-	}
 }
